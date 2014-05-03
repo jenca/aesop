aa1097bff60218ac6f5b7b50facf17629c02d87c
jenca <lajencaku@gmail.com>
2014-05-03 16:57:49 -0700

    updated h2 to h1

--- script.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    for(var i = 0; i < stories.length; i++) {
-        var current_story = stories[i];
-        $('#toc').append('<li><span class="subtext">' + (i+1) + '</span>' + current_story.title + '</li>');
-    }
-})
